a9405206892f75520a97932f5e7f435c9185bd03
gvalenzuela-dev <guillermovalenzuela.dev@gmail.com>
2025-08-30 08:02:11 -0500

    Agregar archivos de proyecto.

---
+++ AppPWA/Program.cs
@@ -0,0 +1,14 @@
+using AppPWA;
+using AppPWA.Services;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<PostService>();
+builder.Services.AddBlazoredLocalStorage();
+await builder.Build().RunAsync();
---
+++ AppPWA/Services/PostService.cs
@@ -0,0 +1,35 @@
+﻿using AppPWA.Models;
+using Blazored.LocalStorage;
+using System.Net.Http.Json;
+
+namespace AppPWA.Services;
+public class PostService(HttpClient httpClient, ILocalStorageService localStorage)
+{
+
+    public async Task<List<Post>> GetPostsAsync()
+    {
+        try
+        {
+            var posts = await httpClient.GetFromJsonAsync<List<Post>>("https://jsonplaceholder.typicode.com/posts");
+
+            if (posts != null)
+            {
+                await localStorage.SetItemAsync("posts", posts);
+            }
+            else
+            {
+                posts = await localStorage.GetItemAsync<List<Post>>("posts");
+            }
+
+
+            return posts ?? new List<Post>();
+        }
+        catch (Exception)
+        {
+
+            return await localStorage.GetItemAsync<List<Post>>("posts");
+        }
+        
+    }
+
+}
